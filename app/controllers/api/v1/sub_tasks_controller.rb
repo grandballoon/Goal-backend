da7edcad8f7dd86951f94c709c09b463be4eaab8
grandballoon <luke.sovern.conley@gmail.com>
2018-09-10 11:39:09 -0400

    fixed subtask create

--- app/controllers/api/v1/sub_tasks_controller.rb
+++ app/controllers/api/v1/sub_tasks_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SubTasksController < ApplicationController
   before_action :find_sub_task, only: [:update, :show, :destroy]
 
  def index
-   @sub_tasks = Task.all
+   @sub_tasks = SubTask.all
    render json: @sub_tasks
  end
 
@@ -15,6 +15,13 @@ class Api::V1::SubTasksController < ApplicationController
    end
  end
 
+ def create
+    @sub_task = SubTask.new(sub_task_params)
+    if @sub_task.save
+      render json: @sub_task
+    end
+  end
+
  def destroy
    @sub_task.destroy
  end
@@ -22,7 +29,7 @@ class Api::V1::SubTasksController < ApplicationController
  private
 
  def sub_task_params
-   params.permit(:title, :description, :user_id, :due_date)
+   params.permit(:description, :task_id, :due_date)
  end
 
  def find_sub_task
