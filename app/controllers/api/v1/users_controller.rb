0959cfa22b73486351f1165a4717e9752c10a5ce
grandballoon <luke.sovern.conley@gmail.com>
2018-09-12 10:47:11 -0400

    added validation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,10 +10,21 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    end
+  end
+
 
   private
 
   def find_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:user_name, :email, :phone_number)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :tasks
   has_many :sub_tasks, through: :tasks
+  validates :user_name, {presence: true, uniqueness: true}
+
 end
