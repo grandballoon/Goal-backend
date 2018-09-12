59ce690033e4b664777bae1b3885ca70e81cec23
jaredianmills <jaredianmills@gmail.com>
2018-09-12 12:19:26 -0400

    added validation and error handling for user sign up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,10 +10,17 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
+
   def create
     @user = User.new(user_params)
     if @user.save
       render json: @user
+    else
+      if params[:user_name] == ''
+        render json: {errors: "Username field must not be blank"}
+      else
+        render json: {errors: "Username already exist"}
+      end
     end
   end
 
